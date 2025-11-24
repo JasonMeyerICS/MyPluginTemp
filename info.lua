4c756939d83c63077bf593a29aa5e24da824fc97
Jason Meyer <35506418+JasonMeyerICS@users.noreply.github.com>
2025-11-24 14:29:43 -0800

    Init
    Init

---
+++ info.lua
@@ -0,0 +1,8 @@
+PluginInfo = {
+  Name = "Base Plugin",
+  Version = "0.0",
+  BuildVersion = "0.0.0.1",
+  Id = "4b263915-2486-4a78-bc8b-172aa46b4725",
+  Author = "Your_Company",
+  Description = "A very basic plugin"  
+}
