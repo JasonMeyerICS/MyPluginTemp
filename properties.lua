b8c036e49c1c1db267934236129aa8af245c3369
Jason Meyer <35506418+JasonMeyerICS@users.noreply.github.com>
2025-11-24 16:39:40 -0800

    Add 'Activate' property to properties.lua

--- properties.lua
+++ properties.lua
@@ -1,6 +1,12 @@
 table.insert(props, {
+  Name = "Activate",
+  Type = "string",
+  Value = "Enter Code Here"
+})
+table.insert(props, {
   Name = "Debug Print",
   Type = "enum",
   Choices = {"None", "Tx/Rx", "Tx", "Rx", "Function Calls", "All"},
   Value = "None"
+
 })
